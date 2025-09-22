66c08d3309114a05c83536945719013ddb3c1f47
dvd3110 <darshan.kkr.3110@gmail.com>
2025-09-22 23:13:19 +0530

    Initial commit

---
+++ my-first-react/src/App.jsx
@@ -0,0 +1,12 @@
+import Welcome from './Welcome'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Darshan 👋</h1>
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-first-react/src/Welcome.jsx
@@ -0,0 +1,10 @@
+function Welcome(){
+    return(
+        <div>
+            <h2> Welcome Component</h2>
+            <p>This is a separate react component.</p>
+        </div>
+    );
+}
+
+export default Welcome
