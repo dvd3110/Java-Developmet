911c6e21554e971b85877258397b220aec0a0ccf
dvd3110 <darshan.kkr.3110@gmail.com>
2025-09-23 22:59:59 +0530

    Revert "Revert "checking for commit restore""
    This reverts commit be4bc5a69c4f9bd3209b4019ee62d866c415a271.

--- my-first-react/src/Welcome.jsx
+++ my-first-react/src/Welcome.jsx
@@ -1,11 +1,12 @@
-function Welcome(){
+function Welcome(props){
     return(
         <div>
             <h2> Welcome Component</h2>
+            <h6>Hello, {props.name}</h6>
             <p>This is a separate react component.</p>
 
 
-            <p>This is the line added to check or learn the branching in git</p>
+            <p>Testing the git commit restore process..</p>
         </div>
     );
 }
